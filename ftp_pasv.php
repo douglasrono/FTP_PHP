991ea1d893e3b21ff768395172bbe9a1b8a539ed
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-13 12:05:13 +0300

    Create ftp_pasv.php

---
+++ ftp_pasv.php
@@ -0,0 +1,24 @@
+ <?php
+// connect and login to FTP server
+$ftp_server = "ftp.example.com";
+$ftp_conn = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");
+$login = ftp_login($ftp_conn, $ftp_username, $ftp_userpass);
+
+// turn passive mode on
+ftp_pasv($ftp_conn, true);
+
+$file = "localfile.txt";
+
+// upload file
+if (ftp_put($ftp_conn, "serverfile.txt", $file, FTP_ASCII))
+  {
+  echo "Successfully uploaded $file.";
+  }
+else
+  {
+  echo "Error uploading $file.";
+  }
+
+// close connection
+ftp_close($ftp_conn);
+?> 
