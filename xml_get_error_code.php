70359b0018a2ba11cbb189d156c77d713b9d8ada
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-15 16:02:34 +0300

    Create xml_get_error_code.php

---
+++ xml_get_error_code.php
@@ -0,0 +1,21 @@
+ <?php
+// Invalid xml file
+$xmlfile = 'test.xml';
+$xmlparser = xml_parser_create();
+
+// Open the file and read data
+$fp = fopen($xmlfile, 'r');
+while ($xmldata = fread($fp, 4096)) {
+  // parse the data chunk
+  if (!xml_parse($xmlparser,$xmldata,feof($fp))) {
+    die( print "ERROR: "
+    . xml_get_error_code($xmlparser)
+    . "<br>Line: "
+    . xml_get_current_line_number($xmlparser)
+    . "<br>Column: "
+    . xml_get_current_column_number($xmlparser)
+    . "<br>");
+  }
+}
+xml_parser_free($xmlparser);
+?> 
